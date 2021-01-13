c6cfa45617e12c8588c0423802ac6d3db610d579
KlaasHagge <klaash@ppedv.de>
2021-01-13 11:56:22 +0100

    Ressourcen und Bindings

---
+++ ColorPickerUebung/DoublesToBrushConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace ColorPickerUebung
+{
+    //MultiBindings benötigen das Interface IMultivalueConverter. Die Convert-Methode empfängt ein value-Array, in welchem die gebundenen Quellen in
+    //derselben Reihenfolge wie in der XAML-Deklaration übergeben werden.
+    class DoublesToBrushConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return new SolidColorBrush(Color.FromArgb((byte)(double)values[3], (byte)(double)values[0], (byte)(double)values[1], (byte)(double)values[2]));
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
