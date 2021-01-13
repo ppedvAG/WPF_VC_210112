4ef5b104060b8e96d65f67dc2d9f15ea9686fb5a
KlaasHagge <klaash@ppedv.de>
2021-01-13 16:35:18 +0100

    TAG 2 (Bindings, Resourcen, Styles, Templates, DataContext) - kommentiert

---
+++ Templates/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Templates
+{
+    public class Person : INotifyPropertyChanged
+    {
+        public string Vorname { get; set; }
+        public string Nachname { get; set; }
+
+        private int alter;
+        public int Alter
+        {
+            get { return alter; }
+            set 
+            { 
+                alter = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Alter)));
+            }
+        }
+
+        public List<DateTime> Adressen { get; set; } = new List<DateTime>()
+        {
+            new DateTime(2003, 12, 3)
+        };
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
