ff4618253f1b3312d3e5ffb9e5122b7078fec4ff
KlaasHagge <klaash@ppedv.de>
2021-01-15 10:19:03 +0100

    Commands + MVVM

---
+++ MVVM/CustomCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace MVVM
+{
+    public class CustomCommand : ICommand
+    {
+        public Action<object> ExecuteMethode { get; set; }
+        public Func<object, bool> CanExecuteMethode { get; set; }
+
+        public CustomCommand(Func<object, bool> can, Action<object> exe)
+        {
+            CanExecuteMethode = can;
+            ExecuteMethode = exe;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return CanExecuteMethode(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            ExecuteMethode(parameter);
+        }
+    }
+}
---
+++ MVVM/ViewModel/StartViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace MVVM.ViewModel
+{
+    public class StartViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public CustomCommand LadeDbCmd { get; set; }
+        public CustomCommand OeffneDbCmd { get; set; }
+
+        public int AnzahlPersonen { get { return Model.Person.Personenliste.Count; } }
+
+        public StartViewModel()
+        {
+            LadeDbCmd = new CustomCommand
+                (
+                    p => AnzahlPersonen == 0,
+
+                    p =>
+                    {
+                        Model.Person.LadePersonenAusDb();
+                        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(AnzahlPersonen)));
+                    }
+                );
+            OeffneDbCmd = new CustomCommand
+                (
+                    p => AnzahlPersonen > 0,
+                    p => { }
+                );
+        }
+    }
+}
